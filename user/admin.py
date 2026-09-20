7688ffb50c6848858dc1f8818e72fec130c9c859
crisjohn.dev.official@gmail.com <crisjohn.dev.official@gmail.com>
2026-09-20 23:53:20 +0800

    new UI

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,110 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+
+    list_display = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+        "role",
+        "is_active",
+        "is_staff",
+        "date_joined",
+    )
+
+    list_filter = (
+        "role",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+        "date_joined",
+    )
+
+    search_fields = (
+        "username",
+        "email",
+        "first_name",
+        "last_name",
+    )
+
+    ordering = (
+        "-date_joined",
+    )
+
+    fieldsets = (
+        (
+            "Account Information",
+            {
+                "fields": (
+                    "username",
+                    "password",
+                )
+            },
+        ),
+        (
+            "Personal Information",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                )
+            },
+        ),
+        (
+            "Role",
+            {
+                "fields": (
+                    "role",
+                )
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important Dates",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
+
+    add_fieldsets = (
+        (
+            "Create User",
+            {
+                "classes": (
+                    "wide",
+                ),
+                "fields": (
+                    "username",
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "role",
+                    "password1",
+                    "password2",
+                    "is_active",
+                    "is_staff",
+                ),
+            },
+        ),
+    )
